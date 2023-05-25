a46cf7ab113ad23620a7ccf1874d8d7f6e27f694
VikiMaiselman <120911332+VikiMaiselman@users.noreply.github.com>
2023-05-25 14:19:11 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,24 @@
-let randomNumber1 = Math.floor(Math.random() * 6) + 1;
-let imageName = "./images/dice";
-document
-  .querySelector("img#first")
-  .setAttribute("src", `${imageName}${randomNumber1}.png`);
+function rollTheDice() {
+  let randomNumber1 = Math.floor(Math.random() * 6) + 1;
+  let imageName = "./images/dice";
+  document
+    .querySelector("img#first")
+    .setAttribute("src", `${imageName}${randomNumber1}.png`);
 
-let randomNumber2 = Math.floor(Math.random() * 6) + 1;
-document
-  .querySelector("img#second")
-  .setAttribute("src", `${imageName}${randomNumber2}.png`);
+  let randomNumber2 = Math.floor(Math.random() * 6) + 1;
+  document
+    .querySelector("img#second")
+    .setAttribute("src", `${imageName}${randomNumber2}.png`);
 
-if (randomNumber1 === randomNumber2) {
-  document.querySelector("h1").innerText = "🚩DRAW!🚩";
-} else if (randomNumber1 > randomNumber2) {
-  document.querySelector("h1").innerText = "🚩Player 1 Wins!";
-} else {
-  document.querySelector("h1").innerText = "🚩Player 2 WIns!";
+  if (randomNumber1 === randomNumber2) {
+    document.querySelector("h1").innerText = "🚩DRAW!🚩";
+  } else if (randomNumber1 > randomNumber2) {
+    document.querySelector("h1").innerText = "🚩Player 1 Wins!";
+  } else {
+    document.querySelector("h1").innerText = "🚩Player 2 WIns!";
+  }
 }
+
+document.querySelector("#check-button").addEventListener("click", function () {
+  rollTheDice();
+});
